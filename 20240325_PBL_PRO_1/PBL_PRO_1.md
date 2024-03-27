09062df3f566cd4a0690c4d4dba53bd9e89c5d5c
annavision21 <annavision3320@naver.com>
2024-03-27 13:36:13 +0900

    Add 20240325 PBL Problem 01 - MNIST

---
+++ 20240325_PBL_PRO_1/PBL_PRO_1.md
@@ -0,0 +1,78 @@
+# L7. Classification Problems
+
+## 0. 문제상황
+
+본인이 입사한 ㈜OO 기업은 외부로부터 문제를 의뢰받아 딥러닝 기반 솔루션을 개발 및 납품하는 기업이다.   
+본인이 입사 후 담당하게 된 첫 업무는 정부기관으로부터 의뢰받은 `수기 숫자 인식(handwritten digit recognition)` 문제이다.   
+
+해당 정부 기관은 다양한 종이 양식에 수기로 작성된 숫자를 인식하여 데이터베이스화하는 작업을 자동화하려고 있다. 이를 위해 필요한 것은 문서 속에서 발견되는 `각각의 수기 숫자 문자`를 `디지털 심볼`로 변환하도록 `인공신경망`을 학습하는 일이다.   
+
+이를 위해서는 `MNIST 데이터베이스` (Modified National Institue of Standards and Technology database)를 활용
+할 것을 요청받았다. 해당 데이터는 미국의 정부 공무원과 고등학생들이 작성한 내용을 바탕으로 수집된 데이터이고, `10가지 숫자`에 대한 `6만개의 표본`을 포함하고 있다. 본인은 몇 주 안에 개발을 완료해야 한다…
+
+## 1. 사용 Package, Module, Class, Functions 설명
+### 1.1. torchvision
+`이미지, 비디오 데이터 처리` 및 `컴퓨터 비전 작업`을 위한 패키지이다.
+#### 1.1.1. (CLASS) torchvision.transforms.Compose(transforms)
+- Intro:  
+  여러 개의 변환을 조합하여 하나의 `전처리 파이프라인`을 만든다.   
+  이 파이프라인은 데이터셋에서 `이미지`를 가져와서 `모델에 입력으로 사용할 수 있는 형식`으로 변환한다.  
+
+- Parameters:  
+  |Param|Type|Desc|
+  |:--:|:--:|:--:|
+  |`transforms`|list of Transform objects|transforms objects의 리스트|
+
+- [문서 바로가기](https://pytorch.org/vision/main/generated/torchvision.transforms.Compose.html)
+
+#### 1.1.2. (CLASS) torchvision.transforms.ToTensor
+- Intro:  
+  이미지를 `Pytorch텐서`로 변환한다. 이미지 데이터의 픽셀 값 범위를 `[0,255]` 에서 `[0.0, 1.0]`으로 `정규화`한다.
+
+- [문서 바로가기](https://pytorch.org/vision/main/generated/torchvision.transforms.ToTensor.html)
+
+#### 1.1.3. (CLASS) torchvision.transforms.Normalize(mean, std, inplace=False)
+- Intro:  
+  `정규화`를 수행한다. 입력 이미지의 각 채널에 대해 `평균을 빼고` `표준편차로 나누어` 픽셀 값을 정규화한다.  
+  모델의 학습을 `안정화`시키고, `성능을 향상`시키는 `전처리 기법`이다.
+
+- Parameters:  
+  |Param|Type|Desc|
+  |:--:|:--:|:--:|
+  |`mean`|sequence|각 채널의 `평균값`|
+  |`std`|sequence|각 채녈의 `표준편차`|
+  |`inplace`|bool,optional|입력 데이터를 수정할 것인지 <br>OR<br> 새로운 텐서를 생성할 것인지|
+
+- [문서 바로가기](https://pytorch.org/vision/main/generated/torchvision.transforms.Normalize.html)
+
+#### 1.1.4. (CLASS) torchvision.datasets.MNIST(root: Union[str, Path], train: bool = True, transform: Optional[Callable] = None, target_transform: Optional[Callable] = None, download: bool = False)
+- Intro:  
+  `MNIST` 데이터셋을 로드하는 PyTorch의 함수이다.  
+  `MNIST`는 `손으로 쓴 숫자 이미지` 데이터셋으로, 컴퓨터 비전 분야에서 많이 사용되는 벤치마크 데이터셋 중 하나이다.
+
+- Parameters:  
+  |Param|Type|Desc|
+  |:--:|:--:|:--:|
+  |`root`|Union[str, Path]|데이터셋이 저장될 `루트 디렉토리의 경로`를 지정합니다. <BR> 데이터셋이 이 디렉토리에 다운로드되거나 저장됩니다.|
+  |`train`|bool, optional|데이터셋이 `훈련 데이터`인지 `테스트 데이터`인지를 지정합니다.<BR> 기본값은 `True`로, 훈련 데이터셋을 로드합니다.|
+  |`transform`|Optional[Callable], optional|이미지 데이터에 적용할 `변환(transform)`을 지정합니다. <BR> 이 매개변수를 통해 이미지 데이터를 `전처리`하거나 변형할 수 있습니다. <BR> 기본값은 `None`으로, 변환을 적용하지 않습니다.|
+  |`target_transform`|Optional[Callable], optional|타겟(레이블) 데이터에 `적용할 변환`을 지정합니다. <BR>예를 들어, 타겟 데이터에 `원-핫 인코딩`을 적용하거나 <BR>특정 형식으로 변환하는 등의 작업을 수행할 수 있습니다. <BR>기본값은 `None`으로, 변환을 적용하지 않습니다.|
+  |`download`|bool, optional|데이터셋을 `다운로드할지 여부`를 지정합니다. <BR>만약 데이터셋이 로컬에 존재하지 않으면 `True`로 설정하면 `자동으로 다운로드`됩니다. <BR>기본값은 `False`로, 다운로드를 수행하지 않습니다.|
+
+- [문서 바로가기](https://pytorch.org/vision/main/generated/torchvision.datasets.MNIST.html)
+
+### 1.2. torch
+#### 1.2.1. (CLASS) torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=None, sampler=None, batch_sampler=None, num_workers=0, collate_fn=None, pin_memory=False, drop_last=False, timeout=0, worker_init_fn=None, multiprocessing_context=None, generator=None, *, prefetch_factor=None, persistent_workers=False, pin_memory_device='')
+- Intro:  
+    `PyTorch`에서 데이터를 `미니배치(mini-batch)`로 분할하고 데이터를 로드하는 역할을 한다.  
+    주로 데이터셋을 반복하면서 모델에 공급할 수 있는 형식으로 데이터를 로드할 때 사용된다. 
+
+- Parameters:  
+  |Param|Type|Desc|
+  |:--:|:--:|:--:|
+  |`dataset`|Dataset|DataLoader에 제공될 `데이터셋`입니다. <br>PyTorch의 `torch.utils.data.Dataset` 클래스를 상속한<br> 사용자 정의 데이터셋 객체를 사용합니다.|
+  |`batch_size`|int, optional|`미니배치`의 크기를 지정합니다. <br>DataLoader는 주어진 배치 크기에 따라 데이터를 미니배치로 나누어 제공합니다.|
+  |`shuffle`|bool, optional|`데이터를 섞을지 여부`를 지정합니다. <br>True로 설정하면 데이터가 에폭마다 섞입니다.|
+  |`생략`|***|***|
+
+- [문서 바로가기](https://pytorch.org/docs/stable/data.html#torch.utils.data.DataLoader)
---
+++ 20240325_PBL_PRO_1/pbl_pro_1.py
@@ -0,0 +1,215 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import sys
+import os
+import inspect
+import time
+import traceback
+from torchvision import datasets, transforms
+from itertools import product
+from datetime import datetime, timedelta
+
+
+"""
+모델 Class 정의
+"""
+# MLP 모델 정의
+class MLP(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, layer_depth, activation_func):
+        super(MLP, self).__init__()
+        layers = []
+
+        for _ in range(layer_depth):
+            layers.append(nn.Linear(input_size, hidden_size))
+            if activation_func == 'relu':
+                layers.append(nn.ReLU())
+            elif activation_func == 'sigmoid':
+                layers.append(nn.Sigmoid())
+            input_size = hidden_size # 다음 input size 업데이트
+
+        layers.append(nn.Linear(hidden_size, output_size))
+        self.model = nn.Sequential(*layers) # 레이어 연결
+            
+
+    def forward(self, x):
+        x = x.view(-1, 28 * 28) # Origin(BatchSize x 28 x 28) => Convert(BatchSize x 784)
+        x = self.model(x)
+        return x
+
+class CNN(nn.Module):
+    pass
+
+
+"""
+함수 정의
+"""
+def get_log_path(app_name):
+    now = datetime.now()
+    weekday = (now.weekday() + 1) % 7
+    sys_path = "./log/"
+
+    if not os.path.exists(sys_path):
+        os.makedirs(sys_path)
+
+    return os.path.join(sys_path, f"{app_name}-{weekday}.log")
+
+
+def log(app_name, content):
+    caller = inspect.currentframe().f_back
+    caller_function = inspect.getframeinfo(caller).function
+    log_path = get_log_path(app_name)
+    date_head = datetime.now().strftime("%m/%d %H:%M:%S")
+
+    try:
+        modified_time = time.localtime(os.path.getmtime(log_path))
+        modified_yday = modified_time.tm_yday
+    except FileNotFoundError:
+        modified_yday = -1
+
+    mode = 'a+' if datetime.now().timetuple().tm_yday == modified_yday else 'w+'
+
+    logmsg = f"[{date_head}] [{caller_function}] {content}\n"
+
+    with open(log_path, mode) as fd:
+        fd.write(logmsg)
+
+def get_loader(batch_size):
+    try:
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5,), (0.5,))
+        ])
+
+        # MNIST DataSet 가져오기(Train/Test)
+        train_dataset = datasets.MNIST(
+            root='./data/', train=True, transform=transform, download=True)
+        test_dataset = datasets.MNIST(
+            root='./data/', train=False, transform=transform)
+
+        # Dataloader: Mini batch를 만들어주는 역할을 함
+        train_loader = torch.utils.data.DataLoader(
+            dataset=train_dataset, batch_size=batch_size, shuffle=True)
+        test_loader = torch.utils.data.DataLoader(
+            dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+        return train_loader, test_loader
+    except Exception as err:
+        raise
+
+
+def train(model, train_loader, criterion, optimizer):
+    try:
+        model.train()  # 훈련모드 설정
+        running_loss = 0.0
+
+        for batch_idx, (data, target) in enumerate(train_loader):
+            optimizer.zero_grad()  # Gradient 초기화(중첩 방지)
+            output = model(data)
+
+            loss = criterion(output, target)  # 손실함수 적용
+            loss.backward()  # 역전파 실행
+
+            optimizer.step()  # 최적화 실행
+            running_loss += loss.item()
+
+        return running_loss / len(train_loader)
+    except Exception as err:
+        raise
+
+
+def test(model, test_loader, criterion):
+    try:
+        model.eval()  # 평가모드 설정
+        test_loss = 0.0
+        correct = 0
+
+        with torch.no_grad():
+            for data, target in test_loader:
+                output = model(data)
+                # 단순 평가이므로 역전파는 실행하지 않음
+                test_loss += criterion(output, target).item()
+
+                """
+                - output(batch_size X 10(0~9))
+                - 행 기준으로 가장 큰 값을 가지는 열의 인덱스를 선택하면, 그 값이 의미하는 바와 일치할 확률이 높음
+                """
+                pred = output.argmax(
+                    dim=1, keepdim=True)  # output에서 가장 큰 값을 가지는 인덱스 선택(행 기준)
+                correct += pred.eq(target.view_as(pred)).sum().item()
+
+        test_loss /= len(test_loader.dataset)  # 테스트 데이터셋 전체에 대한 평균 손실을 의미
+        accuracy = 100. * correct / len(test_loader.dataset)
+        return test_loss, accuracy
+    except Exception as err:
+        raise
+
+
+def Main():
+    try:
+        app_name = "PBL_PRO_1"
+
+        """
+        HyperParameter 설정
+        """
+        num_epochs = [10, 20, 30]
+        batch_sizes = [64, 128, 256]
+        learning_rates = [0.001, 0.0001]
+        layer_depths = [2, 3, 4]
+        hidden_sizes = [256, 512, 1024]
+        activation_funcs = ['relu', 'sigmoid']
+        criterion = nn.CrossEntropyLoss()
+        optimizers = [optim.Adam, optim.SGD]
+        models = ['mlp']
+
+        best_performance = {'accuracy': 0, 'hyperparameters': None, 'model': None}
+
+        total_start_time = time.time()  # 전체 러닝 타임 측정을 위한 시작 시간 기록
+
+        log(app_name, "Start to grid search..!")
+
+        # 모든 하이퍼파라미터 조합(product)에 대해 "그리드 서치" 실행
+        for epoch, batch_size, lr, ld, hs, af, opt, model_type in product(num_epochs, batch_sizes, learning_rates, layer_depths, hidden_sizes, activation_funcs, optimizers, models):
+            log(app_name, f"Training with Epochs: {epoch}, Batch Size: {batch_size}, Learning Rate: {lr}, Layer Depth: {ld}, Hidden Size: {hs}, Activation Function: {af}, Optimizer: {opt.__name__}, Model Type: {model_type}")
+
+            if model_type == 'mlp':
+                model = MLP(input_size=28*28, hidden_size=hs, output_size=10, layer_depth=ld, activation_func=af)
+            elif model_type == 'cnn':
+                continue
+            
+            optimizer = opt(model.parameters(), lr=lr)
+            train_loader, test_loader = get_loader(batch_size)
+
+            epoch_start_time = time.time()  # 현재 실험의 시작 시간 기록
+
+            for epoch_idx in range(1, epoch + 1):
+                train_loss = train(model=model, train_loader=train_loader,
+                                   criterion=criterion, optimizer=optimizer)
+                test_loss, accuracy = test(model, test_loader, criterion)
+                log(app_name, f"Epoch [{epoch_idx}/{epoch}], Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}, Accuracy: {accuracy:.2f}%")
+
+                # 가장 좋은 성능을 달성한 경우 기록
+                if accuracy > best_performance['accuracy']:
+                    best_performance['accuracy'] = accuracy
+                    best_performance['hyperparameters'] = {'Epochs': epoch, 'Batch Size': batch_size, 'Learning Rate': lr, 'Layer Depth': ld, 'Hidden Size': hs, 'Activation Function': af, 'Optimizer': opt.__name__, 'Model Type': model_type}
+                    best_performance['model'] = model
+
+            epoch_end_time = time.time()  # 현재 실험의 종료 시간 기록
+            elapsed_time = timedelta(seconds=epoch_end_time - epoch_start_time)  # 현재 실험의 러닝 타임 계산
+            log(app_name, f"Elapsed Time for Experiment: {elapsed_time}")
+
+        total_end_time = time.time()  # 전체 러닝 타임 측정을 위한 종료 시간 기록
+        total_elapsed_time = timedelta(seconds=total_end_time - total_start_time)  # 전체 러닝 타임 계산
+        log(app_name, f"Total Elapsed Time: {total_elapsed_time}")
+
+        # 가장 좋은 성능을 달성한 하이퍼파라미터와 모델 정보 출력
+        log(app_name, f"Best Performance: Accuracy: {best_performance['accuracy']:.2f}%, Hyperparameters: {best_performance['hyperparameters']}, Model: {best_performance['model']}")
+
+    except Exception as err:
+        traceback_error = traceback.format_exc()
+        log(app_name, traceback_error)
+        sys.exit()
+
+
+if __name__ == "__main__":
+    Main()
